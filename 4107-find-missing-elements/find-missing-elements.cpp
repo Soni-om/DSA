4c11f02434205a6e450fd24c0e1c78db396753ef
Soni-om <omsoni626181@gmail.com>
2026-08-26 22:44:57 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (91.50%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,35 +1,20 @@
 class Solution {
-    bool present(vector<int>& nums, int x){
-        int s = 0;
-        int e = nums.size()-1;
-
-        while(s<=e){
-            int mid = s + (e-s)/2;
-
-            if(nums[mid] == x){
-                return true;
-            }else if(nums[mid] < x){
-                s = mid +1;
-            }
-            else{
-                e = mid-1;
-            }
-        }
-        return false;
-    }
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        unordered_map<bool,int>mp;
-        vector<int>ans;
-        int s = nums[0] + 1;
-        int e = nums[nums.size()-1];
 
-        while(s < e){
-            if(!present( nums,s)){
-                ans.push_back(s);
+        sort(nums.begin(), nums.end());
+        int index = 0;
+        vector<int> ans;
+        int s = nums[0];
+        int n = nums.size() - 1;
+        int e = nums[n];
+
+        for (int i = s; i <= e; i++) {
+            if (i != nums[index]) {
+                ans.push_back(i);
+            } else {
+                index++;
             }
-            s++;
         }
         return ans;
     }
